99094d051c802e1c700b2ccbad23d42159cee3e3
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-11-21 13:33:35 +0530

    KB-12203 Filter workflow status query to return only active workflows

--- src/main/java/org/sunbird/workflow/postgres/repo/WfStatusRepo.java
+++ src/main/java/org/sunbird/workflow/postgres/repo/WfStatusRepo.java
@@ -132,7 +132,7 @@ public interface WfStatusRepo extends JpaRepository<WfStatusEntity, String> {
     @Query(value = "SELECT * FROM wingspan.wf_status WHERE userid = ?1 AND current_status = ?2 AND in_workflow = ?3 AND request_type IN ('GROUP_CHANGE', 'DESIGNATION_CHANGE')", nativeQuery = true)
     List<WfStatusEntity> findByUserIdAndCurrentStatus(String userId, String currentStatus, boolean status);
 
-    @Query(value = "SELECT * FROM wingspan.wf_status WHERE application_id = ?1 AND userid = ?2",
+    @Query(value = "SELECT * FROM wingspan.wf_status WHERE application_id = ?1 AND userid = ?2 AND in_workflow = true",
             nativeQuery = true)
     List<WfStatusEntity> findWorkflowByBatchAndUser(String applicationId, String userId);
 
